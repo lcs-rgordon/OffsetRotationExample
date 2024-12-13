b9e1a031877b1840ea3355de3b919498f9097cbd
Russell Gordon <rgordon@lcs.on.ca>
2024-12-13 15:17:38 -0500

    Initial Commit

---
+++ OffsetRotationExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OffsetRotationExample
+//
+//  Created by Russell Gordon on 2024-12-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OffsetRotationExample/OffsetRotationExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  OffsetRotationExampleApp.swift
+//  OffsetRotationExample
+//
+//  Created by Russell Gordon on 2024-12-13.
+//
+
+import SwiftUI
+
+@main
+struct OffsetRotationExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
